c4c5627fa779a40a9b63a38439712fbe1516c9a9
Jason <jason1234309@gmail.com>
2021-06-02 19:23:29 -0600

    tinkered with the tiletypes.tcl
    Signed-off-by: Jason <jason1234309@gmail.com>

--- tools/tiletypes.tcl
+++ tools/tiletypes.tcl
@@ -26,16 +26,59 @@ proc get_tile_types { tiles} {
     return $types
 }
 
+proc get_site_types { tiles} {
+    set potential_types []
+    foreach T $tiles {
+        set site_type [get_property SITE_TYPE $T]
+        lappend potential_types "SITE_TYPE: "
+        lappend potential_types $site_type
+        set alternate_types {get_property ALTERNATE_SITE_TYPES $T}
+        lappend potential_types "\nALTERNATE_SITE_TYPES: "
+        lappend potential_types $alternate_types
+        lappend potential_types "\n"
+    }
+    return $potential_types
+}
+
+#proc get_design_sites {} {
+#    set sites []
+#    # Get sites for all the cells
+#    foreach C [get_cells] {
+#        set loc [get_property "LOC" $C]
+#        if { $loc != ""} {
+#            set currentSite [get_sites -of $c]
+#            if { [llen $sites] == 0 } {
+#                set sites [get_sites -of $C]
+#            }
+#            else {
+#                lappend sites [get_sites -of $C]
+#            }
+#        }
+#    }
+#    return $sites
+#}
+
+
+
 open_checkpoint [glob *.dcp]
 
 puts "Opened dcp..."
 
 set tile_types [get_tile_types [get_design_tiles]]
 
+set site_types [get_site_types [get_design_tiles]]
+
 set f [open tile_types.lst w]
 
 puts $f "$tile_types"
+puts $f "$site_types"
 
 close $f
 
 puts "Done"
+
+
+
+
+# get_property SITE_TYPE [get_selected_object]
+# get_property ALTERNATE_SITE_TYPES [get_selected_objects]
